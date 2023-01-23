d9d795167247aeb82cf691d59e692d3b3624a3a3
Nesha Mervin <nesha.mervin@gmail.com>
2023-01-23 15:53:31 -0500

    added functionality for adding tasks to an array of objects

--- js/toDoList.js
+++ js/toDoList.js
@@ -1,8 +1,9 @@
 let input = document.querySelector('.to-do-input');
 let message = document.querySelector('#message');
+let tasks = document.querySelector('#to-do-list');
 let clearItemsBtn = document.querySelector('#clear-completed');
 let itemsLeft = 0;
-let data = {};
+let data = [];
 
 input.addEventListener('keypress', e => {
   if (e.key === 'Enter') {
@@ -10,44 +11,28 @@ input.addEventListener('keypress', e => {
 
     formValidation();
   }
-
-  function formValidation() {
-    if (input.value === '') {
-      message.innerText = 'Task cannot be blank';
-      return;
-    } else {
-      message.innerText = '';
-      acceptData();
-      input.value = '';
-    }
-  }
-
-  function acceptData() {
-    data['text'] = input.value;
-    console.log(data);
-  }
-
-  clearItemsBtn.addEventListener('click', removeItems);
-
-  function displayItems(toDoItem) {
-    document.querySelector('#to-do-list').innerHTML += `
-      <li class="to-do-item">
-        <div class="checkbox-container">
-          <button id="completed" class="checkbox" type="button"></button>
-        </div>
-        <p class="to-do">${toDoItem}</p>
-      </li>`;
-  }
-
-  function addToDoItem() {
-    let toDoItem = input.value;
-
-    itemsLeft++;
-    document.querySelector('#items-left').textContent =
-      itemsLeft + ' items left';
-
-    displayItems(toDoItem);
-  }
-
-  function removeItems() {}
 });
+
+function formValidation() {
+  if (input.value === '') {
+    message.innerText = 'Task cannot be blank';
+    return;
+  } else {
+    message.innerText = '';
+    acceptData();
+    input.value = '';
+  }
+}
+
+function acceptData() {
+  data.push({
+    task: input.value,
+  });
+
+  localStorage.setItem('data', JSON.stringify(data));
+  console.log(data);
+
+  displayTasks();
+}
+
+// clearItemsBtn.addEventListener('click', removeItems);
